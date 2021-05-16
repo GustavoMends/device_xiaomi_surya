5eee0495b02728c9a541c0cbd8cde45488dbdce3
dianlujitao <dianlujitao@gmail.com>
2021-05-16 10:07:38 +0200

    surya: Compile vendor variants of vendor.qti.hardware.perf
     [kubersharma001]: Uprev to 2.2
    
    Change-Id: Ia5de8c300d71b6def223e97ef9d58898c9c11a57
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -328,6 +328,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
 PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor \
+    vendor.qti.hardware.perf@2.1.vendor \
     vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
